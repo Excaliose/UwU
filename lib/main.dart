72d153c7aac0dc7599e9eab37976cd5ad1009914
Excaliose <ardiyantosamuel@gmail.com>
2020-02-14 15:23:43 +0700

    upload pertama

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: Rumah(),
+    );
+  }
+}
+
+class Rumah extends StatefulWidget {
+  @override
+  _RumahState createState() => _RumahState();
+}
+
+class _RumahState extends State<Rumah> {
+  AudioCache _audioCache;
+  @override
+  void initState() {
+    super.initState();
+    // create this only once
+    _audioCache = AudioCache(
+        prefix: "audio/",
+        fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("OOF"),
+        ),
+        body: Center(
+          child: Container(
+              child: Stack(children: [
+            Positioned(
+                top: 225,
+                left: 160,
+                child: RaisedButton(
+                  onPressed: () => _audioCache.play('roblox-death-sound_1.mp3'),
+                  child: Text("oof"),
+                ))
+          ])),
+        ));
+  }
+}
